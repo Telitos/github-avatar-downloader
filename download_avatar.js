cab0f835f1c3ecd5e1b1681e42222993dca85202
Ghislain H <telitos@gmail.com>
2016-11-07 22:25:55 +0000

    defined requestURL in getRepoContributor

--- download_avatar.js
+++ download_avatar.js
@@ -1,10 +1,19 @@
-var request = require('request');
+'use strict'
+
+const request = require('request');
+const token = require('./token.json');
+
+const GITHUB_USER = token.username;
+const GITHUB_TOKEN = token.token;
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  let requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  console.log(requestURL);
 }
 
 
