c6390657d5e27166f573872e4f55a5e8f7cffd85
Ghislain H <telitos@gmail.com>
2016-11-08 01:07:11 +0000

    adds command line argurments to download_avatar.js

--- download_avatar.js
+++ download_avatar.js
@@ -1,42 +1,43 @@
 'use strict'
-
+/*we import all our required modules. token.json contains
+my token and username and has been added to .gitignore
+to remain private*/
 const request = require('request');
 const token = require('./token.json');
 const fs = require('fs');
 
 const GITHUB_USER = token.username;
 const GITHUB_TOKEN = token.token;
-
+/*set up a const to get */
+const input = process.argv.slice(2)
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // first we construct the url we want to GET concatenating all the required info to form the proper path
+  // first we construct the url we want to GET by concatenating all the required info to form the proper url
   let requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
-  //Next we make out get request using the url just created, but also using the object {url: 'www.whatevr.com', headers: {'User-Agent': 'whoever'}}
+  //Next we make out get request using the url just created, but also adding the object {url: 'www.whatevr.com', headers: {'User-Agent': 'whoever'}}
   // because that's what gitHub requires.
   request.get({ url: requestURL, headers: { 'User-Agent': 'random' }}, function (err, response, body) {
           let error = err
           if (err) {
             throw err;
           } else if (response.statusCode === 200) {
-            error = "No Errors. Everything went fine."
             let data = JSON.parse(body) // we know the body is JSON so we can parse it right away to pass it to our callback function
             cb(error, data)
             console.log("Data acquired!") // just so we know the process is done
           };
         })
-        //   .on('response', function (response) {                           // This give you various info regarding the response (not necessary)
+        //   .on('response', function (response) {                           // These give you various info regarding the response (not necessary)
         //        console.log('Response Status Code: ', response.statusCode);
         //        console.log('Response Status Message: ', response.statusMessage);
         //        console.log('Response Content Type: ', response.headers['content-type'])
         //      });
         };
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
+getRepoContributors(input[0], input[1], function(err, result) {
 
 // I'll first write a function expression to process each of the avatars' url and download them into the specified path
 
@@ -46,10 +47,11 @@ getRepoContributors("jquery", "jquery", function(err, result) {
     if (err) {
       throw error
     } else if (response.statusCode === 200) {
-      console.log('Download complete!');
+      console.log('Download complete!');            /*This line will print each time our loop completes below, not necessary, but a good
+                                                      visual confirmation that stuff is working*/
     }
     })
-  //   .on ('response', function (response) {
+  //   .on ('response', function (response) {                             // These give you various info regarding the response (not necessary)
   //   console.log('Response Status Code: ', response.statusCode);
   //   console.log('Response Status Message: ', response.statusMessage);
   // })
@@ -57,7 +59,8 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   .pipe(fs.createWriteStream(filePath));
   }
 
-  //Our call back functoin will just loop over the contributors and print out the avatar_url
+  /*Now our callback function will just loop over the contributors to get their avatars' url and specify a pathname for each
+  and pass the outcomes to the downloadImageByURL function*/
 
   result.forEach(function(contributors) {
     let avatarUrl = contributors['avatar_url']
